ad357c2eb2f6b47520fd4f3e1e0abbd95c7b6cff
danaie <el20040@mail.ntua.gr>
2023-05-08 14:11:36 +0300

    Update forms.py

--- lib_flask/forms.py
+++ lib_flask/forms.py
@@ -1,15 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
-
+from wtforms import StringField, PasswordField, SubmitField, DateField , SelectField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class Signup_form(FlaskForm):
 	username = StringField('Username', 
 		validators=[DataRequired(), Length(min=2, max=15)])
 	password = PasswordField('Password', validators=[DataRequired(),Length(max=20)])
-	email = StringField('Email', validators=[DataRequired(),Email()])
-	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
+	first_name = StringField('First Name',
+		validators=[DataRequired(), Length(min=2, max=20)])
+	last_name = StringField('Last Name',
+		validators=[DataRequired(), Length(min=2, max=20)])
+	date_of_birth = DateField('Date of Birth',
+		validators=[DataRequired()])
+	role = SelectField('Role',choices=['Student','Teacher'], validate_choice=True)
+	confirm_password = PasswordField('Confirm Password', 
+		validators=[DataRequired(),EqualTo('password')])
 	submit = SubmitField('Sign Up')
 
 class Login_form(FlaskForm):
@@ -17,3 +23,4 @@ class Login_form(FlaskForm):
 		validators=[DataRequired(), Length(min=2, max=15)])
 	password = PasswordField('Password', validators=[DataRequired(),Length(max=20)])
 	submit = SubmitField('Login')
+
