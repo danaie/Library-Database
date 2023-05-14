3fc65e0f8ce69206e85e7b3c2e9364572af877b5
ajkrit <128644998+ajkrit@users.noreply.github.com>
2023-05-14 14:05:46 +0300

    Update configs.py

--- library/configs.py
+++ library/configs.py
@@ -3,6 +3,14 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '7d08dab940ec78d6b8dd74864dab85f4'
+app.config['SECRET_KEY'] = 'secret'
+app.config["WTF_CSRF_SECRET_KEY"] = ''
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'library'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+
+db = MySQL(app)
 
 from library import routes
