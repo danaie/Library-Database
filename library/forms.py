c9cdc512b6a57a03abec0b052adab0068ce76e2c
danaie <el20040@mail.ntua.gr>
2023-05-15 00:27:42 +0300

    Update forms.py

--- library/forms.py
+++ library/forms.py
@@ -43,3 +43,30 @@ class Book_form(FlaskForm):
 	author_first_name = StringField('Author\'s first name', validators=[DataRequired(), Length(max=20)])
 	author_last_name = StringField('Author\'s last name', validators=[DataRequired(), Length(max=20)])
 	publisher = StringField('Publisher', validators=[DataRequired(), Length(max=20)])
+	
+class School_form(FlaskForm):
+	name = StringField('School Name',
+		validators=[DataRequired(), Length(max=20)])
+	address = StringField('Address',
+		validators=[DataRequired(), Length(max=20)])
+	city = StringField('City',
+		validators=[DataRequired(), Length(max=20)])
+	phone_number = StringField('Phone Number',
+		validators=[DataRequired(), Length(max=10)])
+	email = StringField('email',
+		validators=[DataRequired(), Length(max=50), Email()])
+	principal = StringField('Principal Full Name',
+		validators=[DataRequired(), Length(max=20)])
+	lib_man_first_name = StringField('Library Manager First Name',
+		validators=[DataRequired(), Length(max=20)])
+	lib_man_last_name = StringField('Library Manager Last Name',
+		validators=[DataRequired(), Length(max=20)])
+	lib_man_username = StringField('Libray Manager Username', 
+		validators=[DataRequired(), Length(min=2, max=15)])
+	lib_man_date_of_birth = DateField('Date of Birth',
+		validators=[DataRequired()])
+	password = PasswordField('Password', 
+		validators=[DataRequired(),Length(max=20)])
+	confirm_password = PasswordField('Confirm Password', 
+		validators=[DataRequired(),EqualTo('password')])
+	submit = SubmitField('Add School')
