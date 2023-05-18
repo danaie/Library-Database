8faea783cb9df7619c612e45e04420265795cc2f
ajkrit <128644998+ajkrit@users.noreply.github.com>
2023-05-18 12:25:29 +0300

    Update forms.py

--- library/forms.py
+++ library/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DateField , SelectField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, NumberRange
 
 
 class Signup_form(FlaskForm):
