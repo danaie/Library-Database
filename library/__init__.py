ac26222dbfc674d6b79bd5779cc0f91f367a4d15
Antonis Kritikos <ankrit400@gmail.com>
2023-05-27 19:25:51 +0300

    correct run.py

--- library/__init__.py
+++ library/__init__.py
@@ -4,7 +4,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7d08dab940ec78d6b8dd74864dab85f4'
 app.config["MYSQL_HOST"] = 'localhost'
-app.config["MYSQL_USER"] = "danai"
+app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "Qwer-1234"
 app.config["MYSQL_DB"] = "library"
 db = MySQL(app)
