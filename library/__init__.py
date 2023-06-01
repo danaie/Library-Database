d1574a857a83c0e6c26ea6e7640c8098f9f3e96f
danaie <el20040@mail.ntua.gr>
2023-06-01 13:49:30 +0300

    Update __init__.py

--- library/__init__.py
+++ library/__init__.py
@@ -3,10 +3,10 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7d08dab940ec78d6b8dd74864dab85f4'
-app.config["MYSQL_HOST"] = 'localhost'
-app.config["MYSQL_USER"] = "danai"
-app.config["MYSQL_PASSWORD"] = "Qwer-1234"
-app.config["MYSQL_DB"] = "library"
+app.config["MYSQL_HOST"] = '' #fill your host-name
+app.config["MYSQL_USER"] = '' #fill your username
+app.config["MYSQL_PASSWORD"] = '' #fill your password
+app.config["MYSQL_DB"] = 'library'
 db = MySQL(app)
 
 from library import routes
